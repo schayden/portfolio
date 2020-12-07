f4aff7a09c22432fc4e5cd920a57aaf99d9a77df
schayden <54911628+schayden@users.noreply.github.com>
2020-12-07 15:11:06 -0500

    Update night_mode.js

--- js/night_mode.js
+++ js/night_mode.js
@@ -1,7 +1,38 @@
 var element = document.body;
 var nav = document.getElementById("bbbb");
+var l = document.getElementById("b");
+var i = document.getElementById("iframe")
+
 
 document.querySelector("#night_mode").addEventListener("click", function(){
     element.classList.toggle("night_mode");
-    nav.classList.toggle("night_mode");
+    l.classList.toggle("night_mode");
+    i.classList.add("iframe")
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
